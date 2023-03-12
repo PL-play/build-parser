f11232c6e0579ead2e5fe96877c0b44880940b05
zhangran653 <zhangran42@126.com>
2023-03-12 22:03:43 +0800

    tests

--- test/test_file.py
+++ test/test_file.py
@@ -1,3 +1,5 @@
+import re
+import shlex
 import unittest
 
 
@@ -15,3 +17,19 @@ class LL1Test(unittest.TestCase):
         # points after reading a line
         print(f.tell())
         f.close()
+
+    def test3(self):
+        text = "    {  afsdf zdfa asdf asdfa asdf {asdf} asdf () x*8} }"
+        result = re.findall(r'\{(.*?)\}', text)
+        print(result)
+
+    def test4(self):
+        text = "    {  afsdf zdfa asdf asdfa asdf {asdf} asdf () x*8  x }"
+        text = text.strip()
+        print(text[0], text[-1])
+        print(text[1:-1])
+
+    def test5(self):
+        text = "  }  "
+        test = text.rstrip()
+        print(test)
